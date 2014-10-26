a6f5bb20d135dc97bbab05edcb4e5d8b9050cdc0
Andreas Parusel <andreas.parusel@gmail.com>
2014-10-26 18:58:12 +0100

    created the model.

---
+++ qx-next/fs39exp/source/class/fs39exp/model/Overview.js
@@ -0,0 +1,33 @@
+qx.Class.define("fs39exp.model.Overview",
+{
+
+  include : [qx.event.MEmitter],
+
+  properties:
+  {
+    allExpenses :
+    {
+      // An array with five elements for each weekday
+      check : "qx.data.Array",
+      event : "changeAllExpenses",
+      init : null
+    }
+  },
+
+  members :
+  {
+    update : function(resource)
+    {
+      var result = resource.rows.map(function(element) {
+        return {
+          amount : element.value.amount + " EUR",
+          buyer : element.value.buyer,
+          isCleared : element.value.isCleared,
+          timestamp : fs39exp.util.Date.printPrettyIsoDate(element.value.timestamp),
+          items : element.value.items.join(", ")
+        }
+      });
+      this.allExpenses = new qx.data.Array(result);
+    }
+  }
+});
